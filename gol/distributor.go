fbadf446ab19f9e4600c91fc75cd77ce9d359ac9
JeeIn-Park <1700pji@naver.com>
2022-11-17 18:03:28 +0000

    from concerency step 1

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,25 +14,122 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func calculateNextAliveCells(p Params, world [][]byte) []util.Cell {
+	// takes the current state of the world and completes one evolution of the world
+	// find next alive cells calculating each cell in the given world
+	var aliveCells []util.Cell
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			sum := 0
+			for i := -1; i < 2; i++ {
+				for j := -1; j < 2; j++ {
+					//calculate the number of alive neighbour cells including itself
+					if world[(y+i+p.ImageHeight)%p.ImageHeight][(x+j+p.ImageWidth)%p.ImageWidth] == 0xFF {
+						sum++
+					}
+				}
+			}
+
+			var cell util.Cell
+			cell.X, cell.Y = x, y
+
+			// when the cell was alive in the given world, exclude it from the number of alive neighbour cells
+			// then it keeps alive if it has 2 alive neighbours
+			if world[y][x] == 0xFF {
+				sum = sum - 1
+				if sum == 2 {
+					aliveCells = append(aliveCells, cell)
+				}
+			}
+
+			// when a cell has three alive neighbours, it will be alive anyway
+			if sum == 3 {
+				aliveCells = append(aliveCells, cell)
+			}
+		}
+	}
+
+	return aliveCells
+}
+
+func worldFromAliveCells(p Params, c []util.Cell) [][]byte {
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+
+	for _, i := range c {
+		world[i.Y][i.X] = 0xFF
+	}
+
+	return world
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
+	// Create a 2D slice to store the world.
+	// - get the image in, so we can evolve it with the game of life algorithm (with IO goroutine)
+	// - need to work out the file name from the parameter
+	// - eg. if we had two 256 by 256 coming in,
+	//       we can make out a string and send that down via the appropriate channel
+	//       after we've sent the appropriate command.
+	//       we then get the image byte by byte and store it in this 2D world
 
-	// TODO: Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+	c.ioCommand <- ioInput
+	c.ioFilename <- fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
 
 	turn := 0
+	var aliveCells []util.Cell
+	// - need two 2D slices for this
+	// - get final state of the world as it's evolved
+	// Execute all turns of the Game of Life.
+	// - for loop(call game of life function)
 
+	if p.Turns == 0 {
+		for y := 0; y < p.ImageHeight; y++ {
+			for x := 0; x < p.ImageWidth; x++ {
+				if world[y][x] == 0xFF {
+					var cell util.Cell
+					cell.X, cell.Y = x, y
+					aliveCells = append(aliveCells, cell)
+				}
+			}
+		}
 
-	// TODO: Execute all turns of the Game of Life.
+		world = worldFromAliveCells(p, aliveCells)
+	} else {
+		for i := 0; i < p.Turns; i++ {
+			aliveCells = calculateNextAliveCells(p, world)
+			world = worldFromAliveCells(p, aliveCells)
+			turn++
+		}
 
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+	}
 
+	// Report the final state using FinalTurnCompleteEvent.
+	// - pass it down to events channel
+
+	c.events <- FinalTurnComplete{
+		CompletedTurns: turn,
+		Alive:          aliveCells,
+	}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
