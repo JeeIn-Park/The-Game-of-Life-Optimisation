3b1a2a8a3e9f7b9249985d6492ede4736a4159d9
JeeIn-Park <1700pji@naver.com>
2022-11-18 04:11:22 +0000

    copied and pasted templete from previous lab, add one memo(unversioned before)

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,7 +1,11 @@
 package gol
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"net/rpc"
+	"os"
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -14,68 +18,54 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-func calculateNextAliveCells(p Params, world [][]byte) []util.Cell {
-	// takes the current state of the world and completes one evolution of the world
-	// find next alive cells calculating each cell in the given world
-	var aliveCells []util.Cell
+//parameter: rpc.Client
+//needed when make remote procedure call
+//parameter: message string
+//need to replace with 2d slice and some parameters
 
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			sum := 0
-			for i := -1; i < 2; i++ {
-				for j := -1; j < 2; j++ {
-					//calculate the number of alive neighbour cells including itself
-					if world[(y+i+p.ImageHeight)%p.ImageHeight][(x+j+p.ImageWidth)%p.ImageWidth] == 0xFF {
-						sum++
-					}
-				}
-			}
+// func makeCall(client rpc.Client, message string)
+func makeCall(client *rpc.Client, message string) {
 
-			var cell util.Cell
-			cell.X, cell.Y = x, y
-
-			// when the cell was alive in the given world, exclude it from the number of alive neighbour cells
-			// then it keeps alive if it has 2 alive neighbours
-			if world[y][x] == 0xFF {
-				sum = sum - 1
-				if sum == 2 {
-					aliveCells = append(aliveCells, cell)
-				}
-			}
-
-			// when a cell has three alive neighbours, it will be alive anyway
-			if sum == 3 {
-				aliveCells = append(aliveCells, cell)
-			}
-		}
-	}
-
-	return aliveCells
-}
-
-func worldFromAliveCells(p Params, c []util.Cell) [][]byte {
-	world := make([][]byte, p.ImageHeight)
-	for i := range world {
-		world[i] = make([]byte, p.ImageWidth)
-	}
-
-	for _, i := range c {
-		world[i.Y][i.X] = 0xFF
-	}
-
-	return world
+	request := stubs.Request{Message: message}
+	//use new for this
+	//so this going to be a pointer
+	//it is important for "client.call"
+	response := new(stubs.Response)
+	//when you make remote procedure call,
+	//the response argument needs to be a pointer to itself
+	//stubs.ReverseHandler : tell the remote server which method we're calling
+	//ㄴ SecretStingOperation : registered type
+	//ㄴ Reverse : name of the method
+	//request, response are the argument
+	client.Call(stubs.ReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-	// Create a 2D slice to store the world.
-	// - get the image in, so we can evolve it with the game of life algorithm (with IO goroutine)
-	// - need to work out the file name from the parameter
-	// - eg. if we had two 256 by 256 coming in,
-	//       we can make out a string and send that down via the appropriate channel
-	//       after we've sent the appropriate command.
-	//       we then get the image byte by byte and store it in this 2D world
+	//local controller
+	//responsible for io and capturing keypress
+	//local controller as a client on a local machine
 
+	//client.go = local controller
+
+	//step 1
+	//implementing a basic controller
+	//which can tell the logic engine to evolve game of life
+	//for the number of turns specified in gol.Params.Turns
+	//** can achieve this by implementing a single, blocking RPC call to process all requested turns
+
+	//stubs.go
+	//client uses to call the remote methods on the server
+	//need structure to send request to server and get response back
+	//response struct: a 2d slice (final board state back to local controller)
+	//request struct : a 2d slice (initial state of the board),
+	//				   and other parameters(such as the number of turns, size of image -> so it can iterate the board correctly)
+	//exported method name, exported type (going to be changed to something more appropriate like
+	//									  game of life operations and process turns)
+
+	//loading the board from io
+	//keep this on the client side
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
@@ -90,46 +80,74 @@ func distributor(p Params, c distributorChannels) {
 
 	turn := 0
 	var aliveCells []util.Cell
-	// - need two 2D slices for this
-	// - get final state of the world as it's evolved
-	// Execute all turns of the Game of Life.
-	// - for loop(call game of life function)
 
-	if p.Turns == 0 {
-		for y := 0; y < p.ImageHeight; y++ {
-			for x := 0; x < p.ImageWidth; x++ {
-				if world[y][x] == 0xFF {
-					var cell util.Cell
-					cell.X, cell.Y = x, y
-					aliveCells = append(aliveCells, cell)
-				}
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 0xFF {
+				var cell util.Cell
+				cell.X, cell.Y = x, y
+				aliveCells = append(aliveCells, cell)
 			}
 		}
-
-		world = worldFromAliveCells(p, aliveCells)
-	} else {
-		for i := 0; i < p.Turns; i++ {
-			aliveCells = calculateNextAliveCells(p, world)
-			world = worldFromAliveCells(p, aliveCells)
-			turn++
-		}
-
 	}
 
-	// Report the final state using FinalTurnCompleteEvent.
-	// - pass it down to events channel
+	world = worldFromAliveCells(p, aliveCells)
+
+	// remove for loop
+	// this function and all of sub-functions will be moved to aws
+	// all the turns processed on the remote node and get the result back
+
+	//gol engine
+	//responsible for actual processing the turns of game of life
+	//gol engine as a server on an aws node
+
+	//server.go = game of life worker
 
 	c.events <- FinalTurnComplete{
 		CompletedTurns: turn,
 		Alive:          aliveCells,
 	}
 
-	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
-
 	c.events <- StateChange{turn, Quitting}
-
-	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func main() {
+	//it's fine when you practice with localhost
+	//actually need to use aws node
+	//do not just use local host for the actual submission
+	//server := "127.0.0.1:8030"
+
+	//boilerplate code again
+	//again refers to the golang documentation,
+	//you can directly copy this into assignment
+	//client, err := rpc.Dial("tcp", sever)
+	/*
+		if err != nil {
+			log.Fatal("dialing:", err)
+		}
+		defer client.Close()
+	*/
+
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+	flag.Parse()
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	file, _ := os.Open("wordlist")
+	scanner := bufio.NewScanner(file)
+	//this loop iterating over a text file and sending strings to the server
+	// so that's not the same as the game of life
+	for scanner.Scan() {
+		t := scanner.Text()
+		fmt.Println("Called: " + t)
+		//makeCall(*client, t)
+		//pass the copu of it to makeCall function
+		// client stucture contains mutex lock
+		// copying mutex lock is a problem(two train allowed)
+		makeCall(client, t)
+	}
+
 }
---
+++ server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import "uk.ac.bris.cs/gameoflife/util"
+
+func calculateNextAliveCells(p Params, world [][]byte) []util.Cell {
+	var aliveCells []util.Cell
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			sum := 0
+			for i := -1; i < 2; i++ {
+				for j := -1; j < 2; j++ {
+					if world[(y+i+p.ImageHeight)%p.ImageHeight][(x+j+p.ImageWidth)%p.ImageWidth] == 0xFF {
+						sum++
+					}
+				}
+			}
+
+			var cell util.Cell
+			cell.X, cell.Y = x, y
+
+			if world[y][x] == 0xFF {
+				sum = sum - 1
+				if sum == 2 {
+					aliveCells = append(aliveCells, cell)
+				}
+			}
+
+			if sum == 3 {
+				aliveCells = append(aliveCells, cell)
+			}
+		}
+	}
+
+	return aliveCells
+}
+
+func worldFromAliveCells(p Params, c []util.Cell) [][]byte {
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+
+	for _, i := range c {
+		world[i.Y][i.X] = 0xFF
+	}
+
+	return world
+}
+
+for i := 0; i < p.Turns; i++ {
+aliveCells = calculateNextAliveCells(p, world)
+world = worldFromAliveCells(p, aliveCells)
+turn++
+}
+
