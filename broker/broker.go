d6aa5bdc6d19eba50d9c6a303b6503333425a277
tokkI <kim12ho72@gmail.com>
2022-11-28 18:06:15 +0000

    hi

---
+++ broker/broker.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+type Broker struct{}
+
+var workers = make([]*rpc.Client, 0)
+
+func worldFromAliveCells(c []util.Cell, imageHeight int, imageWidth int) [][]byte {
+	world := make([][]byte, imageHeight)
+	for i := range world {
+		world[i] = make([]byte, imageWidth)
+	}
+	for _, i := range c {
+		world[i.Y][i.X] = 0xFF
+	}
+	return world
+}
+
+func (b *Broker) SendToServer(req stubs.State, res *stubs.State) (err error) {
+	//for i := 0; i < len(req.Ports); i ++ {
+	//	client, _ := rpc.Dial("tcp", req.InstanceIP+":"+req.Ports[i])
+	//	workers = append(workers, client)
+	//}
+
+	//aws := flag.String("port", "127.0.0.1:8050", "AWS IP:PORT")
+	//flag.Parse() // to do : AWS IP:POOR flag 로 받아서 연결
+	//client, _ := rpc.Dial("tcp", *aws)
+
+	//worker = client
+	//
+	//response := new(stubs.State)
+	//call := worker.Go(stubs.EvaluateAllHandler, stubs.State{World: req.World, Turn: req.Turn}, response, nil)
+	//<-call.Done // Get the result of the calculated res.World , res.Turn
+	//// distributor 의 Ticker 함수에 완료 월드 , 턴 반환 request 로
+	//res.World = response.World
+	//res.Turn = response.Turn
+
+	res.World = req.World
+	computingTurn := 0
+	imageHeight := len(res.World)
+	imageWidth := len(res.World[0])
+	numberOfWorkers := len(workers)
+	aliveCellState := make([]*stubs.AliveCellState, numberOfWorkers)
+	for i := 0; i < numberOfWorkers; i++ {
+		aliveCellState[i] = new(stubs.AliveCellState)
+	}
+	//calls := make([]*rpc.Call, len(workers))
+	aliveCells := make([]util.Cell, 0)
+	for y := 0; y < imageHeight; y++ {
+		for x := 0; x < imageWidth; x++ {
+			if res.World[y][x] == 0xFF {
+				var cell util.Cell
+				cell.X, cell.Y = x, y
+				aliveCells = append(aliveCells, cell)
+			}
+		}
+	}
+	for i := 0; i < req.Turn; i++ {
+		aliveCellPart := make([]util.Cell, 0)
+		for n, w := range workers {
+			err := w.Call(stubs.EvaluateOneHandler, stubs.EvaluationRequest{
+				World:          res.World,
+				ID:             n,
+				NumberOfWorker: len(workers),
+			}, aliveCellState[n])
+			if err != nil {
+				fmt.Println()
+			}
+			aliveCellPart = append(aliveCellPart, aliveCellState[n].AliveCells...)
+		}
+		aliveCells = aliveCellPart
+		//for n := range workers {
+		//	<-calls[n].Done
+		//	aliveCells = append(aliveCells, aliveCellState[n].AliveCells...)
+		//}
+		res.World = worldFromAliveCells(aliveCells, imageHeight, imageWidth)
+		computingTurn++
+	}
+	return
+}
+
+//func (b *Broker) TickerToServer(req stubs.None, res *stubs.State) (err error) {
+//	response := new(stubs.State)
+//	call := worker.Go(stubs.TickerHandler, stubs.None{}, response, nil)
+//	<-call.Done
+//	res.World = response.World
+//	res.Turn = response.Turn
+//	return
+//}
+
+//func (b *Broker) KeyPressToServer(req stubs.KeyPress, res *stubs.State) (err error) {
+//	response := new(stubs.State)
+//	call := worker.Go(stubs.KeyPressHandler, stubs.KeyPress{KeyPress: req.KeyPress}, response, nil)
+//	fmt.Println("3-1. Broker : Sending keyPress signal to server ")
+//	<-call.Done
+//	fmt.Println("3-2. Broker : Got state for keyPress signal ")
+//
+//	res.World = response.World
+//	res.Turn = response.Turn
+//
+//	fmt.Println("3-3. Broker : All keyPress states are ready")
+//	return
+//}
+
+//func (b *Broker) ShutDown(req stubs.None, res stubs.None) (err error) {
+//	worker.Go(stubs.ShutDownHandler, stubs.None{}, stubs.None{}, nil)
+//	os.Exit(0)
+//	return
+//}
+
+//func (b *Broker) ReturnFinalState(req stubs.Request, res *stubs.Response) {
+//	byeWorldTurn(res.ComputedWorld, res.CompletedTurn) }
+// server 로 부터 계산 완료된 world 를 (서버 에서 req 로 보냄) 받아 와서 -> distributor 에 전송
+
+func main() {
+	listener, _ := net.Listen("tcp", ":8040")
+	rpc.Register(&Broker{})
+	defer listener.Close()
+
+	client, _ := rpc.Dial("tcp", "127.0.0.1:8050")
+	workers = append(workers, client)
+	//TODO : here needs to be modified to be able to accept multiple servers
+
+	rpc.Accept(listener)
+}
---
+++ server/server.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"net/rpc"
+	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+// remove for loop
+// this function and all of sub-functions will be moved to aws
+// all the turns processed on the remote node and get the result back
+
+//gol engine
+//responsible for actual processing the turns of game of life
+//gol engine as a server on an aws node
+
+//server.go = game of life worker
+
+var (
+	//	nextAddr  string
+	pause     bool
+	tickerC   = make(chan bool)
+	keyPressC = make(chan rune)
+	stateC    = make(chan stubs.State)
+)
+
+func calculateNextAliveCells(world [][]byte, start int, finish int) []util.Cell {
+	var aliveCells []util.Cell
+	imageHeight := len(world)
+	imageWidth := len(world[0])
+
+	for y := start; y < finish; y++ {
+		for x := 0; x < imageHeight; x++ {
+			sum := 0
+			for i := -1; i < 2; i++ {
+				for j := -1; j < 2; j++ {
+					if world[(y+i+imageHeight)%imageHeight][(x+j+imageWidth)%imageWidth] == 0xFF {
+						sum++
+					}
+				}
+			}
+
+			if world[y][x] == 0xFF {
+
+				sum = sum - 1
+				if sum == 2 {
+					aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+				}
+			}
+
+			if sum == 3 {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+
+		}
+	}
+	return aliveCells
+}
+
+func worldFromAliveCells(c []util.Cell, imageHeight int, imageWidth int) [][]byte {
+	world := make([][]byte, imageHeight)
+	for i := range world {
+		world[i] = make([]byte, imageWidth)
+	}
+	for _, i := range c {
+		world[i.Y][i.X] = 0xFF
+	}
+	return world
+}
+
+type GameOfLifeOperation struct{}
+
+//func (g *GameOfLifeOperation) KeyPress(req stubs.KeyPress, res *stubs.State) (err error) {
+//	fmt.Println("Got keyPress from distributor.go correctly")
+//	keyPressC <- req.KeyPress
+//	currentState := <-stateC
+//	res.World = currentState.World
+//	res.Turn = currentState.Turn
+//	fmt.Println("All states are registered correctly")
+//	return
+//}
+//
+//func (g *GameOfLifeOperation) Ticker(req stubs.None, res *stubs.State) (err error) {
+//	tickerC <- true
+//	currentState := <-stateC
+//	res.World = currentState.World
+//	res.Turn = currentState.Turn
+//	return
+//}
+//
+//func (g *GameOfLifeOperation) ShutDown(req stubs.None, res *stubs.None) (err error) {
+//	os.Exit(0)
+//	return
+//}
+
+func (g *GameOfLifeOperation) EvaluateOne(req stubs.EvaluationRequest, res *stubs.AliveCellState) (err error) {
+	imageHeight := len(req.World)
+	id := req.ID
+	numberOfWorkers := req.NumberOfWorker
+
+	size := (imageHeight - (imageHeight % numberOfWorkers)) / numberOfWorkers
+	//remove possibility of remainder
+	//remained world parts will be calculated at the last part
+
+	var aliveCells []util.Cell
+	if id == numberOfWorkers-1 {
+		aliveCells = calculateNextAliveCells(req.World, id*size, imageHeight)
+	} else {
+		aliveCells = calculateNextAliveCells(req.World, id*size, (id+1)*size)
+	}
+	//for y := 0; y < imageHeight; y++ {
+	//	for x := 0; x < imageWidth; x++ {
+	//		if req.World[y][x] == 0xFF {
+	//			var cell util.Cell
+	//			cell.X, cell.Y = x, y
+	//			aliveCells = append(aliveCells, cell)
+	//		}
+	//	}
+	//
+
+	// aliveCells = calculateNextAliveCells(req.World, imageHeight, imageWidth)
+	res.AliveCells = aliveCells
+	return
+}
+
+//func (s *GameOfLifeOperation) EvaluateAll(req stubs.State, res *stubs.State) (err error) {
+//	var aliveCells []util.Cell
+//	res.World = req.World
+//	turn := req.Turn
+//
+//	imageHeight := len(res.World)
+//	imageWidth := len(res.World[0])
+//	for y := 0; y < imageHeight; y++ {
+//		for x := 0; x < imageWidth; x++ {
+//			if res.World[y][x] == 0xFF {
+//				var cell util.Cell
+//				cell.X, cell.Y = x, y
+//				aliveCells = append(aliveCells, cell)
+//			}
+//		}
+//	}
+//	res.Turn = 0
+//
+//	go func() {
+//		for {
+//			select {
+//			case <-tickerC:
+//				stateC <- stubs.State{
+//					World: res.World,
+//					Turn:  res.Turn,
+//				}
+//			case keyPress := <-keyPressC:
+//				switch keyPress {
+//				case 's':
+//					stateC <- stubs.State{
+//						World: res.World,
+//						Turn:  res.Turn,
+//					}
+//				case 'q':
+//					stateC <- stubs.State{
+//						World: res.World,
+//						Turn:  res.Turn,
+//					}
+//				case 'k':
+//					stateC <- stubs.State{
+//						World: res.World,
+//						Turn:  res.Turn,
+//					}
+//					pause = true
+//				case 'p':
+//					func() {
+//						stateC <- stubs.State{
+//							World: res.World,
+//							Turn:  res.Turn,
+//						}
+//						if pause == false {
+//							pause = true
+//						} else if pause == true {
+//							pause = false
+//						}
+//					}()
+//				}
+//
+//			}
+//		}
+//	}()
+//
+//	for i := 0; i < turn; i++ {
+//		for pause {
+//		}
+//		aliveCells = calculateNextAliveCells(res.World, imageHeight, imageWidth)
+//		res.World = worldFromAliveCells(aliveCells, imageHeight, imageWidth)
+//		res.Turn++
+//	}
+//
+//	return
+//}
+
+func main() {
+	pAddr := flag.String("port", "8050", "Port to listen on")
+	//flag.StringVar(&nextAddr, "next", "127.0.0.1:8050", "IP:Port string for next member of the round.")
+	flag.Parse()
+
+	rpc.Register(&GameOfLifeOperation{})
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+
+	defer listener.Close()
+	rpc.Accept(listener)
+}
